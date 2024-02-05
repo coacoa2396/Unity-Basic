4bc2dbfb3509101914c72a2dd3e1853d176c4f7b
Yu_ChanGyu <kgadbcksrb@gmail.com>
2024-02-05 20:25:01 +0900

    feat : 포탑회전각 제한하기 도전중 중간저장

--- Assets/Scripts/TankHeadController.cs
+++ Assets/Scripts/TankHeadController.cs
@@ -8,6 +8,13 @@ public class HeadController : MonoBehaviour
     public Transform turretTransform;
     public float rotateSpeed;
 
+    private Vector3 rot;    // 오일러
+    public float maxXrot;   // 오일러
+    public float minXrot;   // 오일러
+    public float maxYrot;   // 오일러
+    public float minYrot;   // 오일러
+
+
     private Vector3 headDir;
 
     private void Update()
@@ -19,6 +26,33 @@ public class HeadController : MonoBehaviour
     {
         turretTransform.Rotate(Vector3.up, headDir.x * rotateSpeed * Time.deltaTime, Space.World);
         turretTransform.Rotate(Vector3.right, headDir.z * rotateSpeed * Time.deltaTime, Space.Self);
+
+        /*
+        // 회전각 제한
+        rot = turretTransform.rotation.eulerAngles; // 오일러
+        if (rot.x >= maxXrot || rot.x <= minXrot || rot.y >= maxYrot || rot.y <= minYrot)
+        {
+            //rot.x = Mathf.Clamp(rot.x, maxXrot, minXrot);
+            //rot.y = Mathf.Clamp(rot.y, maxYrot, minYrot);
+            if (rot.x >= maxXrot)
+            {
+                rot.x = maxXrot;
+            }
+            else if (rot.x <= minXrot)
+            {
+                rot.x = minXrot;
+            }
+            if (rot.y >= maxYrot)
+            {
+                rot.y = maxYrot;
+            }
+            else if (rot.y <= minYrot)
+            {
+                rot.y = minYrot;
+            }
+            turretTransform.rotation = Quaternion.Euler(rot);
+        }
+        */
     }
 
     private void OnHead(InputValue value)
